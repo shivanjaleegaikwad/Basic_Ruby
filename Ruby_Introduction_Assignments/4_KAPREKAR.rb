8716300ce77f507057f47905d1e55a3d067c5a49
shivanjalee gaikwad <shivanjalee.gaikwad@weboniselab.com>
2021-12-03 19:07:50 +0530

    Kaprekar

---
+++ Ruby_Introduction_Assignments/4_KAPREKAR.rb
@@ -0,0 +1,35 @@
+def convert_array(k)
+  arr = []
+  div = 10**(k.to_s.length - 1)
+
+  while div >= 10 do
+    arr << (k/div)
+    k = (k%div)
+    div = (div / 10)
+  end
+
+  arr << k
+  arr
+end
+
+def kaprekar?(k)
+  c = convert_array(k**2)
+  i = 0
+  result= false
+
+  while i < c.length do
+    if k == (c.first(i).join().to_i + c.last(c.length - i).join().to_i)
+      result = true
+      break
+    end
+    i = i+1
+  end
+result
+end
+  
+  if kaprekar?(9)
+    puts "true"
+  else
+    puts" false"
+  end
+  
