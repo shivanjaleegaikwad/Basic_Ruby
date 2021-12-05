bb61f98302998555babfee17a2d2b29093aa8a61
shivanjalee gaikwad <shivanjalee.gaikwad@weboniselab.com>
2021-12-05 20:49:47 +0530

    Count of even numbers

---
+++ Ruby_OOP_Assignments/1_BLOCK_LAMBDA.rb
@@ -0,0 +1,20 @@
+puts "count of even no in array using block:"
+
+def even_nos(n)
+  n.keep_if { |x| x.even?}.count
+end
+
+ puts even_nos([1,4,6,7])
+puts "\n"
+
+puts "count of even no in array using lambda:"
+
+my_Array = [1,2,3,4,5,6,7,8,9,10]
+my_lambda = lambda do |num|
+ if num % 2 == 0
+return num
+ end
+end
+A= my_Array.count { |a| my_lambda.call(a) }
+puts A
+ 
