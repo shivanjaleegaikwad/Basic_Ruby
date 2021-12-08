e3a903143d4dd87e48e07684ac942d4777a37562
shivanjalee gaikwad <shivanjalee.gaikwad@weboniselab.com>
2021-12-08 13:15:08 +0530

    Changed indentations

--- Ruby_Introduction_Assignments/8_FUEL.rb
+++ Ruby_Introduction_Assignments/8_FUEL.rb
@@ -1,17 +1,17 @@
 class Fuel
-    VAR1=0
-    VAR2=1..10
-    VAR3=11..30
-    VAR4=31..50
-    VAR5=50
+VAR1=0
+VAR2=1..10
+VAR3=11..30
+VAR4=31..50
+VAR5=50
     
-    def show
-        puts "Out of fuel if #{VAR1}"
-        puts "Low,please fill if #{VAR2}"
-        puts "Good for now if #{VAR3}"
-        puts "Almost full if #{VAR4}"
-        puts "Full if #{VAR5}"
-    end
+def show
+puts "Out of fuel if #{VAR1}"
+puts "Low,please fill if #{VAR2}"
+puts "Good for now if #{VAR3}"
+puts "Almost full if #{VAR4}"
+puts "Full if #{VAR5}"
 end
- object = Fuel.new()
- object.show
+end
+object = Fuel.new()
+object.show
