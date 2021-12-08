bf7dc30d732f4d942ccb4d20116a51cb3c993431
shivanjalee gaikwad <shivanjalee.gaikwad@weboniselab.com>
2021-12-08 11:58:30 +0530

    Changed indentation and used .eql

--- Ruby_Introduction_Assignments/3_PALINDROMES.rb
+++ Ruby_Introduction_Assignments/3_PALINDROMES.rb
@@ -1,7 +1,7 @@
 def palindrome?(string)
-   	downcase_stripped_string= string.downcase.gsub(" ", "")
-  	downcase_stripped_string==downcase_stripped_string.reverse
+downcase_stripped_string= string.downcase.gsub(" ", "")
+downcase_stripped_string.eql?downcase_stripped_string.reverse
 end
-
 puts palindrome?('Never odd or even')
 puts "\n"
+
