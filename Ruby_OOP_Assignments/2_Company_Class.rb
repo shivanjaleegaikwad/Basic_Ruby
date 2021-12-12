59b73600b626b52f9684bb5567b93b92aca9b675
shivanjalee gaikwad <shivanjalee.gaikwad@weboniselab.com>
2021-12-12 11:21:55 +0530

    Changed code

--- Ruby_OOP_Assignments/2_Company_Class.rb
+++ Ruby_OOP_Assignments/2_Company_Class.rb
@@ -1,20 +1,27 @@
 class Company
-    def initialize(name)
-        @name = name
-    end
-
-
-def divisions
-    total= no_of_employee
-    puts "This is #{@name} company and have 5 divisions and each division has #{total} number of employees"
+  
+def initialize(name)
+@name = name
 end
 
+def print_name
+puts "Name: #{@name}" 
+end
 
 def no_of_employee
-    200 / 5
-end
+200 / 5
 end
 
-    new_company = Company.new("Webonise")
-    new_company.divisions
-    new_company.no_of_employee
+def employees
+total= no_of_employee
+puts "Company has 5 divisions and Each division has #{total} number of employees"
+end
+
+end
+
+puts "About Company--->"  
+puts "\n"
+new_company = Company.new("Webonise Lab")
+new_company.print_name
+new_company.employees
+new_company.no_of_employee
